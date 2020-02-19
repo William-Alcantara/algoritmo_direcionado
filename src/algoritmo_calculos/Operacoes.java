b850fe2532f4a0eb08c05d12197647ca8e7ca3cd
william-alcantara <wil-alcantara@hotmail.com>
2020-02-19 00:20:54 -0300

    Primeros codigos

--- src/algoritmo_calculos/Operacoes.java
+++ src/algoritmo_calculos/Operacoes.java
@@ -1,5 +1,25 @@
 package algoritmo_calculos;
 
-public class Operacoes {
-
+public class Operacoes {	
+	
+	public double a;
+	public double b;
+	
+	public double areaQuadrado(){
+		return  a * b;//calcula a area do quadrado		
+	}
+	public double areaTriangulo() {
+		return (a*b)/2;//calcula a area do triangulo 
+	}
+	public double areaRetangulo() {
+		return a * b;//calcula a area do retangulo  
+	}
+	public double areaLosagulo() {
+		return (a*b)/2;//no losango é usado D1 e D2 de diagonal maior, a formula é mesma do triangulo 
+	}
+	public double tempFahrenheit(){
+		return a *1.8 + 32;	// converte de Celsius para Fahrenheit  para ao contrario divide por 32	
+	}
+	
+	
 }
--- src/algoritmo_direcionado/Programa.java
+++ src/algoritmo_direcionado/Programa.java
@@ -1,10 +1,26 @@
 package algoritmo_direcionado;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import algoritmo_calculos.Operacoes;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Operacoes operacao = new Operacoes();
+		
+		System.out.println("Digite uma temperatura em Celsius? ");
+		operacao.a = sc.nextDouble();
+		System.out.println("A temperatura Fahrenheith"+operacao.tempFahrenheit());
+		
+		
+		
+		sc.close();
 	}
 
 }
